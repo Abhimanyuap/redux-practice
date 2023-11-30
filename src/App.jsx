11a2215bb7e1cd769de059d09129c3710cf775ff
Abhimanyuap <97864658+Abhimanyuap@users.noreply.github.com>
2023-11-30 19:31:43 +0530

    installed packages and created store.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 
+import { useState } from 'react'
 import './App.css'
 
 function App() {
 
+  const [count, setCount] = useState(0);
+
   return (
     <div className="app">
-      
+      <h1>{count}</h1>
+      <button onClick={() => setCount(count+1)}>+</button>
+      <br />
+      <button onClick={() => setCount(count-1)}>-</button>
     </div>        
   )
 }
